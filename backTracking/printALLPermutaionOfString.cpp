3139791a72863a7d0bfbb273316e3f91c6ca201c
KhushbuSaini-2008 <khushbsaini24.set@modyuniversity.ac.in>
2026-08-09 22:44:41 +0530

    print all permutaion of a string

---
+++ backTracking/printALLPermutaionOfString.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+void printpermutation(string &s,int i){
+    
+    if(i>=s.length()){
+        cout<<s<<endl;
+        return  ;
+    }
+    for(int j=i;j<s.length();j++){
+        swap(s[i],s[j]);
+        printpermutation(s,i+1);
+        swap(s[i],s[j]);
+    }
+   
+}
+int main(){
+    string s="abc";
+    string ans="";
+printpermutation(s,0);
+
+
+  
+    return 0;
+}
